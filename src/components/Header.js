ae59e5f3e5f186e78996465152ab018f9c38a853
khushi gupta <kg834208@gmail.com>
2025-09-03 01:10:32 +0530

    make sign out

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,30 @@
-import React from "react";
-import { LOGO } from "../utils/constants";
+import { useNavigate } from "react-router-dom";
+import { LOGO, USER_AVATAR } from "../utils/constants";
+import { auth } from "../utils/firebase";
+import { signOut } from "firebase/auth";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/");
+      })
+      .catch((error) => {
+      navigate("/error")
+      });
+  };
   return (
-    <div className="absolute px-7 py-1 ml-20 bg-gradient-to-b from-black z-10" >
-      <img className="w-40"
-      src={LOGO} alt="logo" />
+    <div className="absolute w-full  px-7 py-1  bg-gradient-to-b from-black z-10 flex justify-between">
+      <img className="w-40" src={LOGO} alt="logo" />
+      <div className="flex p-2">
+        <img className="w-12 h-12 " src={USER_AVATAR} alt="userIcon" />
+        <button onClick={handleSignOut} className="font-bold text-white">
+          Sign Out
+        </button>
+      </div>
     </div>
   );
 };
