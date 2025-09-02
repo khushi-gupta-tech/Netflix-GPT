4055134de9373f98063da5193ffede4638382079
khushi gupta <kg834208@gmail.com>
2025-09-02 18:38:25 +0530

    sign in/sign up form && Login/header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './index';
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="text-3xl font-bold text-green-600">
-      Namaste 
-    </div>
-  );
+  return  <Body/>
 }
 
 export default App;
