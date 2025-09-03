d8c1c6b18fe404bb2578a8a51460a8bbe135e852
khushi gupta <kg834208@gmail.com>
2025-09-04 01:42:21 +0530

    video done

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -7,12 +7,12 @@ const VideoBackground = ({ movieId }) => {
     useMovieTrailer(movieId);
 
   return (
-    <div>
+    <div className="w-screen">
       <iframe
-        width="560"
-        height="315"
-        src={"https://www.youtube.com/embed/9bqIwMH3En8?si="+trailerVideo?.key}
+        className="w-screen aspect-video"
+        src={"https://www.youtube.com/embed/qH6SqkWS9WA?si="+trailerVideo?.key + "?&autoplay=1&mute=1"}
         title="YouTube video player"
+
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
     </div>
