5b52240b7e68a764ef9c94d6f32a0ffa1026957b
khushi gupta <kg834208@gmail.com>
2025-09-04 00:45:57 +0530

    dispatch  action

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../utils/movieSlice";
 
 const VideoBackground = ({ movieId }) => {
-
-    const [trailerId,setTrailerId] = useState(null);
-
+    const dispatch = useDispatch();
+    const trailerVideo = useSelector((store)=> store.movie?.trailerVideo)
   // fetch trailer video
   const getMovieVideos = async () => {
     const data = await fetch(
@@ -17,7 +18,7 @@ const VideoBackground = ({ movieId }) => {
     const filterData = json.results.filter((video) => video.type === "Trailer");
     const trailer = filterData.length ? filterData[0] : json.results;
     console.log(trailer);
-    setTrailerId(trailer.key);
+    dispatch(addTrailerVideo(trailer));
   };
 
   useEffect(() => {
@@ -29,7 +30,7 @@ const VideoBackground = ({ movieId }) => {
       <iframe
         width="560"
         height="315"
-        src={"https://www.youtube.com/embed/9bqIwMH3En8?si="+trailerId}
+        src={"https://www.youtube.com/embed/9bqIwMH3En8?si="+trailerVideo?.key}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
