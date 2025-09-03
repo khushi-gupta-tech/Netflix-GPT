d7a652dc034f90a326f51f2ff68eb46bf2cafd8e
khushi gupta <kg834208@gmail.com>
2025-09-04 00:37:20 +0530

    fetch background video

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { API_OPTIONS } from "../utils/constants";
 
-const VideoBackground = () => {
+const VideoBackground = ({ movieId }) => {
+
+    const [trailerId,setTrailerId] = useState(null);
+
+  // fetch trailer video
+  const getMovieVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/1242011/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    console.log(json);
+
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results;
+    console.log(trailer);
+    setTrailerId(trailer.key);
+  };
+
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
+
   return (
-    <div>VideoBackground</div>
-  )
-}
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/9bqIwMH3En8?si="+trailerId}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
+};
 
-export default VideoBackground
+export default VideoBackground;
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const VideoTitle = ({title,overview}) => {
   return (
-    <div>
-        <h1>{title}</h1>
-        <h1>{overview}</h1>
-        <button>Play</button>
-        <button>More Info</button>
+    <div className='pt-36 px-12'>
+        <h1 className='text-6xl font-bold'>{title}</h1>
+        <p className='py-6  text-lg w-1/4'>{overview}</p>
+        <button className='bg-gray-500  text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg'>▶️ Play</button>
+        <button className='bg-gray-500 mx-2 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg'>More Info</button>
     </div>
   )
 }
