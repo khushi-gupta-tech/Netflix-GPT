d7de66614ebd46de6c06691c3b9b2b9f5a5f9fd4
khushi gupta <kg834208@gmail.com>
2025-09-04 23:26:02 +0530

    fix the css

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -8,7 +8,7 @@ const SecondaryContainer = () => {
   return (
     movies.nowPlayingMovies && (
     <div className='bg-black'>
-      <div className='-mt-64 pl-10  relative z-20'>
+      <div className='-mt-60 pl-10  relative z-20'>
       <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
       <MovieList title={"Top Rated"} movies={movies.topRatedMovies}/>
       <MovieList title={"Popular"} movies={movies.popularMovies}/>
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const VideoTitle = ({title,overview}) => {
   return (
-    <div className='w-screen aspect-video pt-[18%] px-24 absolute text-white bg-gradient-to-r from-black'>
+    <div className='w-screen aspect-video pt-[16%] px-24 absolute text-white bg-gradient-to-r from-black'>
         <h1 className='text-5xl font-bold'>{title}</h1>
         <p className='py-6  text-md w-1/4'>{overview}</p>
-        <button className='bg-white  text-black px-4 py-3 px-12 text-xl rounded-lg hover:bg-opacity-80'>▶️ Play</button>
-        <button className='bg-gray-500 mx-2 text-white py-3 px-4 text-xl bg-opacity-50 rounded-lg'>More Info</button>
+        <button className='bg-white  text-black px-2 py-2  text-lg rounded-lg hover:bg-opacity-80'>▶️ Play</button>
+        <button className='bg-gray-500 mx-2 text-white py-2 px-2 text-lg bg-opacity-50 rounded-lg'>More Info</button>
     </div>
   )
 }
