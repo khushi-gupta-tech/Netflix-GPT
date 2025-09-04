ac09401607b17ba230b99172f2eafa202f121ee4
khushi gupta <kg834208@gmail.com>
2025-09-04 18:15:26 +0530

    add popular movies list

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -11,7 +11,7 @@ const SecondaryContainer = () => {
       <div className='-mt-64 pl-10  relative z-20'>
       <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
       <MovieList title={"Trending"} movies={movies.nowPlayingMovies}/>
-      <MovieList title={"Popular"} movies={movies.nowPlayingMovies}/>
+      <MovieList title={"Popular"} movies={movies.popularMovies}/>
       <MovieList title={"Upcoming Movies"} movies={movies.nowPlayingMovies}/>
       <MovieList title={"Horror"} movies={movies.nowPlayingMovies}/>
       </div>
