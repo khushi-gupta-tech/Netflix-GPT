064c64b41ef8726a1a4a0f6ac5aa9cfcafc07397
khushi gupta <kg834208@gmail.com>
2025-09-05 21:09:02 +0530

    done multilnguage

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,16 +1,19 @@
 import React from "react";
 import lang from "../utils/languageConstants";
+import { useSelector } from "react-redux";
 
 const GptSearchBar = () => {
+
+    const langKey = useSelector(Store => Store.config.lang || 'en' );
   return (
     <div className="pt-[10%] flex justify-center ">
       <form className="w-1/2 grid grid-cols-12">
         <input
           type="text"
           className="p-2 m-4  col-span-9 rounded-lg"
-          placeholder={lang.hindi.gptSearchPlaceholder}
+          placeholder={lang[langKey]?.gptSearchPlaceholder}
         />
-        <button className=" col-span-3 m-4  p-2 px-4 bg-red-700 text-white rounded-lg">{lang.hindi.search}</button>
+        <button className=" col-span-3 m-4  p-2 px-4 bg-red-700 text-white rounded-lg">{lang[langKey]?.search}</button>
       </form>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ import { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { addUser, removeUser } from "../utils/userSlice";
 import { toggleGptSearchView } from "../utils/gptSlice";
-
+import { changeLanguage } from "../utils/configSlice";
 const Header = () => {
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
@@ -24,7 +24,7 @@ const Header = () => {
   };
 
   useEffect(() => {
-  const unsubscribe =  onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         // sign in
         const { uid, email, displayName, photoURL } = user;
@@ -46,25 +46,42 @@ const Header = () => {
 
     // UNSUBSCRIBE  when component unmounts
     return () => unsubscribe(); // cleanup
-  }, [dispatch,navigate]);
+  }, [dispatch, navigate]);
 
-  const handleGptSearch = () =>{
+  const handleGptSearch = () => {
     // Toggle GPT Search
     dispatch(toggleGptSearchView());
-  }
+  };
 
+  const handleLangChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
   return (
     <div className="absolute w-full  px-7 py-1  bg-gradient-to-b from-black z-10 flex justify-between">
       <img className="w-40" src={LOGO} alt="logo" />
       {user && (
         <div className="flex p-2 items-center">
-          <select className="p-2 m-2 bg-gray-900 text-white">
-            {SUPPORTED_LANG.map((lang) =><option key={lang.identifier} vlaue={lang.identifier}>{lang.name}</option> )}
+          <select
+            className="p-2 m-2 bg-gray-900 text-white"
+            onChange={handleLangChange}
+          >
+            {SUPPORTED_LANG.map((lang) => (
+              <option key={lang.identifier} value={lang.identifier}>
+                {lang.name}
+              </option>
+            ))}
           </select>
-          <button className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
-          onClick={handleGptSearch}>GPT Search</button>
+          <button
+            className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
+            onClick={handleGptSearch}
+          >
+            GPT Search
+          </button>
           <img className="w-10 h-10 mr-2" src={user?.photoURL} alt="userIcon" />
-          <button onClick={handleSignOut} className="font-bold text-black bg-white rounded-lg p-2 hover:bg-gray-300">
+          <button
+            onClick={handleSignOut}
+            className="font-bold text-black bg-white rounded-lg p-2 hover:bg-gray-300"
+          >
             Sign Out
           </button>
         </div>
