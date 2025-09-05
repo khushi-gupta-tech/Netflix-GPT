5deb2fd60c82d88b31871d96d968cc24a99cc747
khushi gupta <kg834208@gmail.com>
2025-09-05 19:12:18 +0530

    add background

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,12 +1,20 @@
-import GptSearchBar from './GptSearchBar'
+import GptSearchBar from "./GptSearchBar";
 import GptMovieSuggestions from "./GptMovieSuggestions";
+import { BG_URL } from "../utils/constants";
 const GptSearch = () => {
   return (
     <div>
-        <GptSearchBar/>
-        <GptMovieSuggestions/>
+      <div className="absolute  -z-10">
+        <img
+          src={BG_URL}
+          alt="background"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <GptSearchBar />
+      <GptMovieSuggestions />
     </div>
-  )
-}
+  );
+};
 
 export default GptSearch;
