b723ea7fb5f147a629c9466072d3e6c4b6c2ea01
khushi gupta <kg834208@gmail.com>
2025-09-06 02:33:39 +0530

    secure the api

--- src/components/GptMovieSuggestions.js
+++ src/components/GptMovieSuggestions.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-
+import MovieCart from './MovieCart'
 const GptMovieSuggestions = () => {
 
   const searchMovies = useSelector(store => store.gpt.gptMovies)
   console.log(searchMovies);
+
+  if (!searchMovies) return null;
   return (
-    <div>
-      
+    <div className='flex flex-wrap w-32 p-4 m-4 bg-black bg-opacity-90'>
+     {searchMovies.map((movie)=> <MovieCart key={movie.id} posterPath={movie.poster_path} /> )}
     </div>
   )
 }
